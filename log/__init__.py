e328199ee98bc3fab89cff6caf1e38156c84ecbe
liuhaoranzuiniubi <34928350+liuhaoranzuiniubi@users.noreply.github.com>
2020-07-20 09:39:38 +0800

    增加了log模块功能

--- log/__init__.py
+++ log/__init__.py
@@ -1,13 +1,13 @@
 import logging
 import logging.handlers
+import time
 
-
-def getLog(path="log\logfile\log.log"):
+def getLog(path="log\logtest\log.log"):
     log_file = path
     logger = logging.getLogger("m_id")
     logger.setLevel(level=logging.INFO)
 
-    formatter = logging.Formatter('%(asctime)s-%(levelname)s-%(message)s')
+    formatter = logging.Formatter('%(asctime)s/%(levelname)s/%(message)s')
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
@@ -25,3 +25,12 @@ def getLog(path="log\logfile\log.log"):
 
 # global logger
 logger=getLog()
+
+def log_file(id):
+    now = time.time()
+    local_time = time.localtime(now)
+    filename = time.strftime('%Y-%m-%d %H-%M-%S', local_time)
+    date_format_localtime = time.strftime('%Y-%m-%d %H:%M:%S', local_time)
+    print("log:"+id)
+    with open('E:\\Desktop\\MovieRecommendationSystem\\log\\logfile\\' + filename + '.txt', mode='a', encoding='utf-8') as file:
+        file.write(date_format_localtime+","+str(id))
